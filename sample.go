62fab4ac76bcf749a62d34f0dee2ad205b9e1c7c
jarwonozt <jarwonoztaveiro@gmail.com>
2024-05-02 02:01:14 +0700

    feat: sample menu

--- sample.go
+++ sample.go
@@ -1,7 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Println("Helo world sample")
+	for {
+		fmt.Println("Menu:")
+		fmt.Println("1. Pilihan 1")
+		fmt.Println("2. Pilihan 2")
+		fmt.Println("3. Keluar")
+
+		var pilihan int
+		fmt.Print("Pilih menu: ")
+		_, err := fmt.Scanln(&pilihan)
+		if err != nil {
+			fmt.Println("Error:", err)
+			os.Exit(1)
+		}
+
+		switch pilihan {
+		case 1:
+			fmt.Println("Anda memilih Pilihan 1")
+		case 2:
+			fmt.Println("Anda memilih Pilihan 2")
+		case 3:
+			fmt.Println("Keluar dari program")
+			os.Exit(0)
+		default:
+			fmt.Println("Pilihan tidak valid")
+		}
+	}
 }
