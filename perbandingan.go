fd40c309126e0a14de8b434c49ec48c1ff48910b
jarwonozt <jarwonoztaveiro@gmail.com>
2024-05-03 14:03:41 +0700

    feat: add perbandingan, operator_perbandingan

---
+++ perbandingan.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var name1 = "Jarwonozt"
+	var name2 = "Jarwonozt"
+
+	var x = 5
+	var y = 7
+
+	var result bool = name1 == name2 // True
+	var resultInteger bool = x > y   // False
+
+	fmt.Println(result)
+	fmt.Println(resultInteger)
+
+}
